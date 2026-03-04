b05d8b4b02955905922f63f9e815a207acc126e5
WillCampbell-git <CAMPBEWW@DUKES.JMU.EDU>
2026-03-04 15:04:17 -0500

    Yup

--- Module 3 - Time-Series Data/4 - Assignments/3.4.1 - Drop Jump/drop-jump-template.py
+++ Module 3 - Time-Series Data/4 - Assignments/3.4.1 - Drop Jump/drop-jump-template.py
@@ -31,12 +31,13 @@ def main(full_path_to_file):
     sampling_rate = 1000
 
     # Step 1: Establish a baseline by examining the force data the after for first ~20 points
+    
 
     # set an amount of time to average and find the baseline
-    baseline_length = 0 ### your code here ###
+    baseline_length = 20 ### your code here ###
 
     # over the baseline, determine the average signal value
-    baseline = 0 ### your code here ###
+    baseline = np.mean(force_plate[:baseline_length]) ### your code here ###
 
     # Step 2: After the baseline, find the first point that rises above that value
     # given some acceptable delta
@@ -62,7 +63,7 @@ def main(full_path_to_file):
             # mark this index as the landing point
 
             ### your code here ###
-
+            first_landing_index = index
             # break out of the loop to end iterating
             break
 
@@ -82,10 +83,14 @@ def main(full_path_to_file):
 
     # walk through the list but start a few moments after the at the landing index
     # since we know the take off point will be afterwards.
-    for index in range(first_landing_index + 10, len(force_plate_list)):
+    for index in range(first_landing_index + 100, len(force_plate_list)):
 
         ### your code here ###
-        delete_me = 0
+        value = force_plate_list[index]
+
+        if abs(value - baseline) <= delta:
+            take_off_index = index
+            break
 
 
     # Step 4: The plate should remain near baseline while the user is in the air (there is no load).
@@ -102,15 +107,19 @@ def main(full_path_to_file):
     for index in range(take_off_index + 10, len(force_plate_list)):
 
         ### your code here ###
-        delete_me = 0
+        value = force_plate_list[index]
+
+        if value > baseline + delta:
+            second_landing_index = index
+            break
 
     # Step 5: calculate the time of contact on plate and time of flight in air
 
     # calculate tc and convert to seconds using the sampling rate
-    time_of_contact = 0 ### your code here ###
+    time_of_contact = (take_off_index - first_landing_index) / sampling_rate ### your code here ###
 
     # calculate tf and convert to seconds using the sampling rate
-    time_of_flight = 0 ### your code here ###
+    time_of_flight = (second_landing_index - take_off_index) / sampling_rate ### your code here ###
 
     # Step 6: Calculate the Reactive Strength Index
 
@@ -118,7 +127,7 @@ def main(full_path_to_file):
     g = constants.g
 
     # RSI = (g*tf^2) / (8*tc)
-    RSI = 0 ### your code here ###
+    RSI = (g*(time_of_flight**2)) / (8*time_of_contact) ### your code here ###
 
     ### Do not modify below this line ###
 
@@ -136,7 +145,7 @@ if __name__ == "__main__":
     filename = "FP1.txt"
 
     # load force plate data (this path may change based upon where you place this file in your project)
-    path_to_data_folder = "../../../data/drop-jump/force-plate/"
+    path_to_data_folder = r"C:\Users\willi\Desktop\ENGR 315\ENGR315-sp2026-student\data\drop-jump\force-plate\\"
 
     ### Do not modify below this line ###
 
--- Module 3 - Time-Series Data/4 - Assignments/3.4.2 - Tensile Testing/tensile-strength-step2.py
+++ Module 3 - Time-Series Data/4 - Assignments/3.4.2 - Tensile Testing/tensile-strength-step2.py
@@ -60,9 +60,16 @@ def calculate_stress(force, sample_diameter):
     :return: An array of stresses experienced by the sample in Kilo Pascals (MPa)
     """
 
-    ### YOUR SOLUTION FROM STEP 1 TEMPLATE HERE ###
+    # calculate the cross-section area (mm^2)
+    ### your code here ###
+    cross_sectional_area = np.pi*(sample_diameter/2)**2
+    # calculate stress (MPa) from load (kN) and cross-sectional area
+    ### your code here ###
 
-    return None
+    # delete this line and replace it with your own
+    stress = force/cross_sectional_area
+
+    return stress
 
 
 def calculate_max_strength_strain(strain, stress):
@@ -76,10 +83,9 @@ def calculate_max_strength_strain(strain, stress):
     """
 
     # calculate the maximum stress experienced
-    ultimate_tensile_stress = -1
-
+    ultimate_tensile_stress = np.max(stress)
     # calculate the maximum strain experienced
-    fracture_strain = -1
+    fracture_strain = np.max(strain)
 
     return ultimate_tensile_stress, fracture_strain
 
@@ -97,7 +103,7 @@ if __name__ == "__main__":
 
     ### Do not modify below this line ###
 
-    path_to_directory = "../../../data/tensile/"
+    path_to_directory = r"C:\Users\willi\Desktop\ENGR 315\ENGR315-sp2026-student\data\tensile\\"
     path_to_samples = path_to_directory + material_folder + "/"
 
     # manually parse file to get gage diameter and then calculate cross-sectional area
